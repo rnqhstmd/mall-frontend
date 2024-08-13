222d72dc98827f20e569a09903cef04201097a68
BonSeung <bs.koo@sqisoft.com>
2024-08-13 14:56:40 +0900

    feat: todo api axios 설정 추가 - #2

---
+++ src/api/todoApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const API_SERVER_HOST = 'http://localhost:8080';
+const prefix = `${API_SERVER_HOST}/todo`;
+
+export const getOne = async (tno) => {
+    const res = await axios.get(`${prefix}/${tno}`);
+    return res.data;
+};
+
+export const getList = async (pageParam) => {
+    const { page, size } = pageParam;
+    const res = await axios.get(`${prefix}`, { params: { page: page, size: size } });
+    return res.data;
+};
