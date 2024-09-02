dac6fab908745bb706a0b13c1bf7e7d835d1a917
BonSeung <bs.koo@sqisoft.com>
2024-09-02 15:34:21 +0900

    feat: 상품 추가 컴포넌트 추가 - #6

---
+++ src/pages/product/AddPage.js
@@ -0,0 +1,10 @@
+import AddComponent from '../../componets/products/AddComponent';
+const AddPage = () => {
+    return (
+        <div className="p-4 w-full bg-white">
+            <div className="text-3xl font-extrabold">Products Add Page</div>
+            <AddComponent />
+        </div>
+    );
+};
+export default AddPage;
